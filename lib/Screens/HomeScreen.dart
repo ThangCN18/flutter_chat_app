21512923298b2c1c7ecd05a5d5924c7a44e9aa1c
huanlv620 <huanliendoanh@gmail.com>
2022-03-27 23:45:53 +0700

    class_

--- lib/Screens/HomeScreen.dart
+++ lib/Screens/HomeScreen.dart
@@ -8,3 +8,8 @@ import 'package:flutter_chat_app/group_chats/group_chat_screen.dart';
 
 import 'ChatRoom.dart';
 
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
