318a22088048ba15691058413ef26d40efe41cd8
ThangCN18 <thang49082@donga.edu.vn>
2022-03-28 00:03:00 +0700

    Register

---
+++ lib/Authenticate/CreateAccount.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'Method.dart';
+import '../Screens/HomeScreen.dart';
+
+class CreateAccount extends StatefulWidget {
+  @override
+  _CreateAccountState createState() => _CreateAccountState();
+}
+
