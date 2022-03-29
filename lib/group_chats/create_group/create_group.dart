08e019d896bc334cf899d176b5c85e4fc15ba8d5
vietdoan10 <vietdp10@gmail.com>
2022-03-29 20:53:08 +0700

    create_group2

--- lib/group_chats/create_group/create_group.dart
+++ lib/group_chats/create_group/create_group.dart
@@ -12,4 +12,53 @@ class CreateGroup extends StatefulWidget {
   State<CreateGroup> createState() => _CreateGroupState();
 }
 
+class _AddMembersINGroupState extends State<AddMembersINGroup> {
+  final TextEditingController _search = TextEditingController();
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  Map<String, dynamic>? userMap;
+  bool isLoading = false;
+  List membersList = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    membersList = widget.membersList;
+  }
+
+  void onSearch() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    await _firestore
+        .collection('users')
+        .where("email", isEqualTo: _search.text)
+        .get()
+        .then((value) {
+      setState(() {
+        userMap = value.docs[0].data();
+        isLoading = false;
+      });
+      print(userMap);
+    });
+  }
+
+  void onAddMembers() async {
+    membersList.add(userMap);
+
+    await _firestore.collection('groups').doc(widget.groupChatId).update({
+      "members": membersList,
+    });
+
+    await _firestore
+        .collection('users')
+        .doc(userMap!['uid'])
+        .collection('groups')
+        .doc(widget.groupChatId)
+        .set({"name": widget.name, "id": widget.groupChatId});
+  }
+
+  
+}
 //
