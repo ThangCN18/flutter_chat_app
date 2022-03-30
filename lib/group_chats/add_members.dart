e3e3a126a83f5c83c78402536d62c0f17f896dab
huanlv620 <huanliendoanh@gmail.com>
2022-03-30 12:19:28 +0700

    add_members.dart

---
+++ lib/group_chats/add_members.dart
@@ -0,0 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class AddMembersINGroup extends StatefulWidget {
+  final String groupChatId, name;
+  final List membersList;
+  const AddMembersINGroup(
+      {required this.name,
+      required this.membersList,
+      required this.groupChatId,
+      Key? key})
+      : super(key: key);
+
+  @override
+  _AddMembersINGroupState createState() => _AddMembersINGroupState();
+}
+
+
