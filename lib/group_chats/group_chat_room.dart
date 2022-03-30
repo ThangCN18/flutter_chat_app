89ecd49e5450f0e5bb7975f95c5dede3a0324405
unknown <tuan48594@donga.edu.vn>
2022-03-30 12:00:50 +0700

    group chat room p1

---
+++ lib/group_chats/group_chat_room.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+
+class GroupChatRoom extends StatelessWidget {
+  final String groupChatId, groupName;
+
+  GroupChatRoom({required this.groupName, required this.groupChatId, Key? key})
+      : super(key: key);
+
+  final TextEditingController _message = TextEditingController();
+
+  void onSendMessage() async {
+    if (_message.text.isNotEmpty) {
+      Map<String, dynamic> chatData = {
+        "sendBy": "",
+        "message": _message.text,
+        "type": "text",
+        "time": FieldValue.serverTimestamp(),
+      };
+
+      _message.clear();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(groupName),
+        actions: [
+          IconButton(
+              onPressed: () => Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (_) => GroupInfo(
+                        groupName: groupName,
+                        groupId: groupChatId,
+                      ),
+                    ),
+                  ),
+              icon: Icon(Icons.more_vert)),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: size.height / 1.27,
+              width: size.width,
+              child: StreamBuilder<QuerySnapshot>(
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return ListView.builder(
+                      itemCount: snapshot.data!.docs.length,
+                      itemBuilder: (context, index) {
+                        Map<String, dynamic> chatMap =
+                            snapshot.data!.docs[index].data()
+                                as Map<String, dynamic>;
+
+                        return messageTile(size, chatMap);
+                      },
+                    );
+                  } else {
+                    return Container();
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget messageTile(Size size, Map<String, dynamic> chatMap) {
+    return Builder(builder: (_) {
+      if (chatMap['type'] == "text") {
+        return Container(
+          width: size.width,
+          child: Container(
+              padding: EdgeInsets.symmetric(vertical: 8, horizontal: 14),
+              margin: EdgeInsets.symmetric(vertical: 5, horizontal: 8),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: Colors.blue,
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    chatMap['sendBy'],
+                    style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(
+                    height: size.height / 200,
+                  ),
+                  Text(
+                    chatMap['message'],
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              )),
+        );
+      } else if (chatMap['type'] == "img") {
+        return Container();
+      } else if (chatMap['type'] == "notify") {
+        return Container();
+      } else {
+        return SizedBox();
+      }
+    });
+  }
+}
