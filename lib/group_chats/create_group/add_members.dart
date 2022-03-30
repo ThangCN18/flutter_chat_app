31a896d154a0f6b95c0019d7df562bc543ee41a3
vietdoan10 <vietdp10@gmail.com>
2022-03-30 13:17:43 +0700

    add_members

---
+++ lib/group_chats/create_group/add_members.dart
@@ -0,0 +1,106 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import 'create_group.dart';
+
+class AddMembersInGroup extends StatefulWidget {
+  const AddMembersInGroup({Key? key}) : super(key: key);
+
+  @override
+  State<AddMembersInGroup> createState() => _AddMembersInGroupState();
+}
+
+class _AddMembersInGroupState extends State<AddMembersInGroup> {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Add Members"),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Flexible(
+              child: ListView.builder(
+                itemCount: membersList.length,
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    onTap: () => onRemoveMembers(index),
+                    leading: Icon(Icons.account_circle),
+                    title: Text(membersList[index]['name']),
+                    subtitle: Text(membersList[index]['email']),
+                    trailing: Icon(Icons.close),
+                  );
+                },
+              ),
+            ),
+            SizedBox(
+              height: size.height / 20,
+            ),
+            Container(
+              height: size.height / 14,
+              width: size.width,
+              alignment: Alignment.center,
+              child: Container(
+                height: size.height / 14,
+                width: size.width / 1.15,
+                child: TextField(
+                  controller: _search,
+                  decoration: InputDecoration(
+                    hintText: "Search",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: size.height / 50,
+            ),
+            isLoading
+                ? Container(
+                    height: size.height / 12,
+                    width: size.height / 12,
+                    alignment: Alignment.center,
+                    child: CircularProgressIndicator(),
+                  )
+                : ElevatedButton(
+                    onPressed: onSearch,
+                    child: Text("Search"),
+                  ),
+            userMap != null
+                ? ListTile(
+                    onTap: onResultTap,
+                    leading: Icon(Icons.account_box),
+                    title: Text(userMap!['name']),
+                    subtitle: Text(userMap!['email']),
+                    trailing: Icon(Icons.add),
+                  )
+                : SizedBox(),
+          ],
+        ),
+      ),
+      floatingActionButton: membersList.length >= 2
+          ? FloatingActionButton(
+              child: Icon(Icons.forward),
+              onPressed: () => Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => CreateGroup(
+                    membersList: membersList,
+                  ),
+                ),
+              ),
+            )
+          : SizedBox(),
+    );
+  }
+}
