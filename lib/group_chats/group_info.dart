333312d768448360a36576e33e00c15e0fb8d27c
RonNguyen166 <ronnguyen165@gmail.com>
2022-03-30 16:00:51 +0700

    group info

---
+++ lib/group_chats/group_info.dart
@@ -0,0 +1,179 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+
+class GroupInfo extends StatefulWidget {
+  final String groupId, groupName;
+  const GroupInfo({required this.groupId, required this.groupName, Key? key})
+      : super(key: key);
+
+  @override
+  State<GroupInfo> createState() => _GroupInfoState();
+}
+
+class _GroupInfoState extends State<GroupInfo> {
+  List membersList = [];
+  bool isLoading = true;
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
+  @override
+  void initState() {
+    super.initState();
+
+  }
+  bool checkAdmin() {
+    bool isAdmin = false;
+
+    membersList.forEach((element) {
+      if (element['uid'] == _auth.currentUser!.uid) {
+        isAdmin = element['isAdmin'];
+      }
+    });
+    return isAdmin;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+
+    return SafeArea(
+      child: Scaffold(
+        body: isLoading
+            ? Container(
+          height: size.height,
+          width: size.width,
+          alignment: Alignment.center,
+          child: CircularProgressIndicator(),
+        )
+            : SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Align(
+                alignment: Alignment.centerLeft,
+                child: BackButton(),
+              ),
+              Container(
+                height: size.height / 8,
+                width: size.width / 1.1,
+                child: Row(
+                  children: [
+                    Container(
+                      height: size.height / 11,
+                      width: size.height / 11,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.grey,
+                      ),
+                      child: Icon(
+                        Icons.group,
+                        color: Colors.white,
+                        size: size.width / 10,
+                      ),
+                    ),
+                    SizedBox(
+                      width: size.width / 20,
+                    ),
+                    Expanded(
+                      child: Container(
+                        child: Text(
+                          widget.groupName,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                            fontSize: size.width / 16,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              //
+
+              SizedBox(
+                height: size.height / 20,
+              ),
+
+              Container(
+                width: size.width / 1.1,
+                child: Text(
+                  "${membersList.length} Members",
+                  style: TextStyle(
+                    fontSize: size.width / 20,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+
+              SizedBox(
+                height: size.height / 20,
+              ),
+
+              // Members Name
+
+              checkAdmin()
+                  ? ListTile(
+                // navigate
+                onTap: () {},
+                leading: Icon(
+                  Icons.add,
+                ),
+                title: Text(
+                  "Add Members",
+                  style: TextStyle(
+                    fontSize: size.width / 22,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              )
+                  : SizedBox(),
+
+              Flexible(
+                child: ListView.builder(
+                  itemCount: membersList.length,
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      onTap: () {},
+                      leading: Icon(Icons.account_circle),
+                      title: Text(
+                        membersList[index]['name'],
+                        style: TextStyle(
+                          fontSize: size.width / 22,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      subtitle: Text(membersList[index]['email']),
+                      trailing: Text(
+                          membersList[index]['isAdmin'] ? "Admin" : ""),
+                    );
+                  },
+                ),
+              ),
+
+              ListTile(
+                onTap: (){},
+                leading: Icon(
+                  Icons.logout,
+                  color: Colors.redAccent,
+                ),
+                title: Text(
+                  "Leave Group",
+                  style: TextStyle(
+                    fontSize: size.width / 22,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.redAccent,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
