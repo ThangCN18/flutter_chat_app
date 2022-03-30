437dc0a234071616ba14b651e2774e20315c2127
huanlv620 <huanliendoanh@gmail.com>
2022-03-30 13:35:03 +0700

    add_members_Widget build

--- lib/group_chats/add_members.dart
+++ lib/group_chats/add_members.dart
@@ -47,7 +47,6 @@ class _AddMembersINGroupState extends State<AddMembersINGroup> {
   }
 
   void onAddMembers() async {
-
     membersList.add(userMap);
 
     await _firestore.collection('groups').doc(widget.groupChatId).update({
@@ -62,6 +61,67 @@ class _AddMembersINGroupState extends State<AddMembersINGroup> {
         .set({"name": widget.name, "id": widget.groupChatId});
   }
 
- 
+  @override
+  Widget build(BuildContext context) {
+    
+    final Size size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Add Members"),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            SizedBox(
+              height: size.height / 20,
+            ),
+            Container(
+              height: size.height / 14,
+              width: size.width,
+              alignment: Alignment.center,
+              child: Container(
+                height: size.height / 14,
+                width: size.width / 1.15,
+                child: TextField(
+                  controller: _search,
+                  decoration: InputDecoration(
+                    hintText: "Search",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: size.height / 50,
+            ),
+            isLoading
+                ? Container(
+                    height: size.height / 12,
+                    width: size.height / 12,
+                    alignment: Alignment.center,
+                    child: CircularProgressIndicator(),
+                  )
+                : ElevatedButton(
+                    onPressed: onSearch,
+                    child: Text("Search"),
+                  ),
+            userMap != null
+                ? ListTile(
+                    onTap: onAddMembers,
+                    leading: Icon(Icons.account_box),
+                    title: Text(userMap!['name']),
+                    subtitle: Text(userMap!['email']),
+                    trailing: Icon(Icons.add),
+                  )
+                : SizedBox(),
+          ],
+        ),
+      ),
+    );
+  }
 }
 
